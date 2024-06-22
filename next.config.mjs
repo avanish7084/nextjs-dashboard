67e954fda1ecb7b8c48f56834dacc7beafb2230d
Avanish.Dubey <avanish.dubey@nw18.com>
2024-06-22 22:19:06 +0530

    Fix configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
-// next.config.js
-module.exports = {
+// next.config.mjs
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     reactStrictMode: true,
     // other configurations...
-  };
-  
+};
+
+export default nextConfig;
